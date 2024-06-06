66bc0f8147701c30626eb80735b89d0fa28256c9
Игнат Игнат <Genezis2022@outlook.com>
2024-06-06 15:28:34 +0300

    рефактор рич модели

--- AutoParts.Domain/Entities/Product.cs
+++ AutoParts.Domain/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using AutoParts.Domain.Base;
 using AutoParts.Domain.ValueObjects;
+using CSharpFunctionalExtensions;
 
 namespace AutoParts.Domain.Entities
 {
@@ -16,10 +17,14 @@ namespace AutoParts.Domain.Entities
 
         public string Manufacture {  get; set; } = string.Empty;
 
-        public ProductName Name {  get; set; }
+        public string Name { get; set; }= string.Empty;
+        public string VendorName { get; set; } = string.Empty;
 
-        public Product (bool state,bool inStock, DateTime dateArrial, string categoy, string description, string manufacture, ProductName name)
+  
+
+        private Product (bool state,bool inStock, DateTime dateArrial, string categoy, string description, string manufacture, string name, string vendorName)
         {
+            Id = Guid.NewGuid ();
             State = state;
             InStock = inStock;
             DateArrival = dateArrial;
@@ -27,6 +32,42 @@ namespace AutoParts.Domain.Entities
             Description = description;
             Manufacture = manufacture;
             Name = name;
+            VendorName = vendorName;
         }
+
+        public static Result<Product> Create(bool state, bool inStock, DateTime dateArrial, string categoy, string description, string manufacture, string name, string vendorName)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return Result.Failure<Product>($"{nameof(name)} не может быть пустым");
+
+            }
+
+            if (string.IsNullOrEmpty(vendorName))
+            {
+                return Result.Failure<Product>($"{nameof(vendorName)} не может быть пустым");
+            }
+
+            if (string.IsNullOrEmpty(categoy))
+            {
+                return Result.Failure<Product>($"{nameof(categoy)} не может быть пустым");
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                return Result.Failure<Product>($"{nameof(description)} не может быть пустым");
+            }
+
+            if (string.IsNullOrEmpty(manufacture))
+            {
+                return Result.Failure<Product>($"{nameof(manufacture)} не может быть пустым");
+            }
+
+            var product = new Product(state, inStock, dateArrial, categoy, description, manufacture, name, vendorName);
+
+            return Result.Success(product);
+        }
+
+
     }
 }
